f3ff29119e574d28b07de97d7c69c031fdbfc29d
arikhativa <49197152+arikhativa@users.noreply.github.com>
2022-12-10 16:36:46 +0100

    BuiltinNoSignalBug (#99)
    Co-authored-by: Yoav Rabby <yrabby@c2r7s3.42madrid.com>

--- include/executer.h
+++ include/executer.h
@@ -6,7 +6,7 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 12:20:11 by yoav              #+#    #+#             */
-/*   Updated: 2022/12/09 10:23:34 by yrabby           ###   ########.fr       */
+/*   Updated: 2022/12/09 14:33:24 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,9 @@ t_error_code	executer_run_cmd(t_shell_op *sp, t_cmd *c);
 void			executer_child_logic(t_shell_op *sp, t_cmd *c);
 t_error_code	executer_run_all_cmds(t_shell_op *sp);
 t_error_code	executer_run_builtin(t_shell_op *sp, t_cmd *c);
-t_error_code	run_single_builtin(t_shell_op *sp, t_cmd *c);
+t_error_code	run_single_builtin(t_shell_op *sp);
 void			hndl_child_signal_exit(int *ret, int sig);
+t_bool			is_forks_in_cmd(t_shell_op *sp);
+t_error_code	executer_run_many_cmds(t_shell_op *sp);
 
 #endif
