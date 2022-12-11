ff9f4172b46051c505301b593f8c33273eca69a9
arikhativa <49197152+arikhativa@users.noreply.github.com>
2022-12-11 18:42:22 +0100

    SignalsOnPipeLine (#117)
    Co-authored-by: Yoav Rabby <yrabby@c2r7s3.42madrid.com>

--- src/executer/executer_many_cmds.c
+++ src/executer/executer_many_cmds.c
@@ -6,11 +6,34 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 12:19:47 by yoav              #+#    #+#             */
-/*   Updated: 2022/12/03 08:50:00 by yrabby           ###   ########.fr       */
+/*   Updated: 2022/12/11 12:19:48 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executer.h"
+
+static void	hndl_last_cmd(t_shell_op *sp, t_dll *n)
+{
+	int		stt;
+	t_cmd	*c;
+
+	c = n->value;
+	if (OK != c->stt)
+		sp->last_cmd_stt = c->stt;
+	else
+	{
+		waitpid(c->pid, &stt, 0);
+		if (WIFEXITED(stt))
+		{
+			if (is_builtin(cmd_get_cmd(c)))
+				sp->last_cmd_stt = c->builtin_ret_val;
+			else
+				sp->last_cmd_stt = WEXITSTATUS(stt);
+		}
+		else if (WIFSIGNALED(stt))
+			hndl_child_signal_exit(&(sp->last_cmd_stt), stt);
+	}
+}
 
 // TODO make sure all builtins return "builtin_ret_val"
 static void	wait_all_cmds(t_shell_op *sp)
@@ -20,26 +43,14 @@ static void	wait_all_cmds(t_shell_op *sp)
 	t_cmd	*c;
 
 	n = cmd_list_get_list(shell_op_get_cmd_list(sp));
-	while (n)
+	while (n->next)
 	{
 		c = n->value;
-		if (OK != c->stt)
-			sp->last_cmd_stt = c->stt;
-		else
-		{
+		if (OK == c->stt)
 			waitpid(c->pid, &stt, 0);
-			if (WIFEXITED(stt))
-			{
-				if (is_builtin(cmd_get_cmd(c)))
-					sp->last_cmd_stt = c->builtin_ret_val;
-				else
-					sp->last_cmd_stt = WEXITSTATUS(stt);
-			}
-			else if (WIFSIGNALED(stt))
-				hndl_child_signal_exit(&(sp->last_cmd_stt), stt);
-		}
 		n = cmd_list_get_next_cmd(n);
 	}
+	hndl_last_cmd(sp, n);
 }
 
 t_error_code	executer_run_many_cmds(t_shell_op *sp)
