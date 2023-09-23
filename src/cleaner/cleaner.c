a46d3bc7daeca63abe019b9d5b5942071abfd553
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2023-09-23 17:10:32 +0200

    delete

--- src/cleaner/cleaner.c
+++ src/cleaner/cleaner.c
@@ -6,16 +6,17 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 14:04:03 by yoav              #+#    #+#             */
-/*   Updated: 2022/12/14 16:19:56 by yrabby           ###   ########.fr       */
+/*   Updated: 2023/09/23 17:10:21 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cleaner.h"
 
-void	cleaner_round_clean(t_shell_op *sp)
+void cleaner_round_clean(t_shell_op *sp)
 {
 	if (sp->input)
 	{
+		tab_deep_destroy(&(sp->input));
 		tab_deep_destroy(&(sp->input));
 		token_list_destroy(&(sp->token_list));
 		cmd_list_destroy(&(sp->cmd_list));
@@ -23,7 +24,7 @@ void	cleaner_round_clean(t_shell_op *sp)
 	}
 }
 
-void	cleaner_on_parse_error(t_shell_op *sp)
+void cleaner_on_parse_error(t_shell_op *sp)
 {
 	if (sp->input)
 	{
